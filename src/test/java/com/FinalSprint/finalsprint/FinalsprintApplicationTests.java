e8fd8ab0c7e8d9b61c70300ace43ad2f3ffcfe26
MarkCrocker <71830279+MarkCrocker@users.noreply.github.com>
2021-12-07 14:29:48 -0330

    Adding master branch

---
+++ src/test/java/com/FinalSprint/finalsprint/FinalsprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FinalSprint.finalsprint;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalsprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
